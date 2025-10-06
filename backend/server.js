2ffe6fb6b954e99063237870cdd684df82cae477
ANWedage <IT23826304@my.sliit.lk>
2025-10-07 02:20:54 +0530

    remove uploades

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-// Serve uploaded files statically
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 // Connect to MongoDB
 connectDB(process.env.MONGODB_URI);
